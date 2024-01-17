745f6fad5fc4d43399fd3d69ff91d00c48f91a2a
liviagalletti <liligalletti@gmail.com>
2024-01-17 20:51:11 -0300

    home

--- script/getdates.js
+++ script/getdates.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
     var currentYearSpan = document.getElementById("current-year");
     if (currentYearSpan) {
         currentYearSpan.textContent = new Date().getFullYear();
     }
 
-    
     var lastModifiedParagraph = document.getElementById("lastModified");
     if (lastModifiedParagraph) {
         lastModifiedParagraph.textContent = "Last modified: " + document.lastModified;
