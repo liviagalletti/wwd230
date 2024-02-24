6880b7a43f255d50600634e4a897e501e964863f
liviagalletti <liligalletti@gmail.com>
2024-02-24 14:48:58 -0300

    .

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -26,3 +26,13 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('show');
 });
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
