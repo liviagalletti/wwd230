d4b5b2b58b850ae5d506afa7ccd7300dff1d6924
liviagalletti <liligalletti@gmail.com>
2024-03-04 16:59:37 -0300

    JOIN

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -36,3 +36,30 @@ if (numVisits !== 0) {
 }
 numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
+
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Obtenha o elemento de input oculto
+        var timestampInput = document.getElementById('timestamp');
+        
+        // Obtenha o tempo atual em milissegundos
+        var currentTime = Date.now();
+        
+        // Defina o valor do campo de entrada oculto como o tempo atual
+        timestampInput.value = currentTime;
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Get the form element
+        var form = document.getElementById('myForm');
+
+        // Add event listener for form submission
+        form.addEventListener('submit', function(event) {
+            // Prevent the default form submission
+            event.preventDefault();
+
+            window.location.href = 'thankyou.html';
+        });
+    });
+
