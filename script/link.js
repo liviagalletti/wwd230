497b7731d767eaf83ead80af6ef971fb7fd9da78
liviagalletti <liligalletti@gmail.com>
2024-03-06 14:02:03 -0300

    json

---
+++ script/link.js
@@ -0,0 +1,32 @@
+const baseUrl = "https://liviagalletti.github.io/wdd230/";
+const linkUrl = "https://liviagalletti.github.io/wdd230/data/links.json";
+
+async function getLinks () {
+    const response = await fetch (linkUrl);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
+
+function displayLinks(weeks) {
+    const linksContainer = document.querySelector('#links');
+
+    weeks.forEach(week => {
+        const weekTitle = document.createElement('h2');
+        weekTitle.textContent = `Week ${week.week}`;
+        linksContainer.appendChild(weekTitle);
+
+        const linksList = document.createElement('ul');
+        week.links.forEach(link => {
+            const listItem = document.createElement('li');
+            const linkElement = document.createElement('a');
+            linkElement.textContent = link.title;
+            linkElement.setAttribute('href', link.url);
+            listItem.appendChild(linkElement);
+            linksList.appendChild(listItem);
+        });
+
+        linksContainer.appendChild(linksList);
+    });
+}
