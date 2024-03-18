f19db82881135a81ed5bec6a28397efe7144f11f
liviagalletti <liligalletti@gmail.com>
2024-03-18 12:00:08 -0300

    chamber

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,52 @@
+// Select HTML elements in the document
+const apiKey = "3c931e01b408736408c6cbb8823da90e";
+const lat =  -7.115;
+const lon = -34.8631;  
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    const location =    document.querySelector("#location");
+    const currentTemp = document.querySelector("#current-temp");
+    const weatherIcon = document.querySelector("#weather-icon");
+    const captionDesc = document.querySelector("#weather-description");
+    const feelsLike =   document.querySelector("#feels-like");
+    const windSpeed =   document.querySelector("#wind-speed");
+    const humidity =    document.querySelector("#humidity");
+
+
+    location.innerHTML = data.name;
+    // Format temperature to show zero decimal points
+    const formattedTemp = data.main.temp.toFixed(0);
+    // Display current temperature
+    currentTemp.innerHTML = `${formattedTemp}&deg;F`;
+    feelsLike.innerHTML = `${data.main.feels_like.toFixed(0)}&deg;F`;
+    windSpeed.innerHTML = `${data.wind.speed.toFixed(0)}mph`;
+    humidity.innerHTML = `${data.main.humidity}%`;
+
+    // Display weather icon and description
+    data.weather.forEach((weatherEvent) => {
+        const iconsrc = `https://openweathermap.org/img/wn/${weatherEvent.icon}@2x.png`;
+        let desc = weatherEvent.description;
+        weatherIcon.setAttribute("src", iconsrc);
+        weatherIcon.setAttribute("alt", desc);
+        captionDesc.innerHTML = `${desc}`;
+    });
+}
+
