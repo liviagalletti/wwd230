22fbd10ed1230882927f17711938964c72131ccf
liviagalletti <liligalletti@gmail.com>
2024-03-21 13:29:47 -0300

    ham

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,32 +1,3 @@
-// JavaScript for toggling dark mode
-const modeButton = document.querySelector("#mode");
-const body = document.body;
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-
-    if (body.classList.contains("dark-mode")) {
-        main.style.background = "#000";
-        main.style.color = "#fff";
-        modeButton.textContent = "🔆";
-    } else {
-        main.style.background = "#eee";
-        main.style.color = "#000";
-        modeButton.textContent = "🕶️";
-    }
-});
-
-
-// Get the menu button and navigation element
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-// Add event listener to toggle navigation visibility
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-});
-
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 if (numVisits !== 0) {
