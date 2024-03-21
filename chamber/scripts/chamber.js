cb231fae93bfb6099189c6cba6cf404cdf07e7e6
liviagalletti <liligalletti@gmail.com>
2024-03-21 18:24:36 -0300

    js

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,36 +1,37 @@
-const visitsDisplay = document.querySelector(".visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `Welcome! Let us know if you have any questions`;
-}
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
+document.addEventListener('DOMContentLoaded', function() {
+    const visitsDisplay = document.querySelector(".visits");
+    let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+    
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = `Welcome! Let us know if you have any questions`;
+    }
+    
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the form element
+    var form = document.getElementById('myForm');
 
+    // Add event listener for form submission
+    form.addEventListener('submit', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Obtenha o elemento de input oculto
-        var timestampInput = document.getElementById('timestamp');
-        
-        // Obtenha o tempo atual em milissegundos
-        var currentTime = Date.now();
-        
-        // Defina o valor do campo de entrada oculto como o tempo atual
-        timestampInput.value = currentTime;
+        window.location.href = 'thankyou.html';
     });
+});
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Get the form element
-        var form = document.getElementById('myForm');
-
-        // Add event listener for form submission
-        form.addEventListener('submit', function(event) {
-            // Prevent the default form submission
-            event.preventDefault();
-
-            window.location.href = 'thankyou.html';
-        });
-    });
-
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the element for displaying the current year
+    var currentYearElement = document.getElementById('current-year');
+    
+    // Get the current year
+    var currentYear = new Date().getFullYear();
+    
+    // Set the current year in the HTML
+    currentYearElement.textContent = currentYear;
+});
