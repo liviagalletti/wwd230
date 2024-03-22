f71dae1d31868f5770ab7520e2f5d1352fa99cb0
liviagalletti <liligalletti@gmail.com>
2024-03-22 15:02:38 -0300

    last modified

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -8,27 +8,22 @@ document.addEventListener("DOMContentLoaded", function () {
     if (lastModifiedParagraph) {
         lastModifiedParagraph.textContent = "Last modified: " + document.lastModified;
     }
+
+    const mainnav = document.querySelector('.navigation')
+    const hambutton = document.querySelector('#menu');
+
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('show');
+        hambutton.classList.toggle('show');
+    });
+
+    const visitsDisplay = document.querySelector(".visits");
+    let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+    }
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
 });
-
-
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
-});
-
-
-const visitsDisplay = document.querySelector(".visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
-}
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
-
-
