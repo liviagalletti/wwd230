757eaf196303f76690eac4e53f5d316aece91a41
liviagalletti <liligalletti@gmail.com>
2024-04-10 14:07:57 -0300

    banner

--- Scoots/scripts/weather.js
+++ Scoots/scripts/weather.js
@@ -207,3 +207,36 @@ const apiKey = "3c931e01b408736408c6cbb8823da90e";
             const date = new Date(timestamp);
             return date.toLocaleDateString("en-US", options);
         }
+
+// banner
+
+async function fetchAndDisplayBanner(apiKey, lat, lon) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayBanner(data);
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Function to display the banner
+function displayBanner(data) {
+    const maxTemp = data.main.temp_max.toFixed(0);
+    document.getElementById('maxTemp').textContent = maxTemp;
+    document.getElementById('scootsBanner').style.display = 'block';
+}
+
+// Function to close the banner
+function closeBanner() {
+    document.getElementById('scootsBanner').style.display = 'none';
+}
+
+// Call the fetchAndDisplayBanner function to fetch and display the banner
+fetchAndDisplayBanner(apiKey, lat, lon);
